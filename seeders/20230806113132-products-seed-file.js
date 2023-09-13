e9c25483a1dc2a52d5d0bb3264772ddd938c7a3f
Jasmineeds <jasmine12122005@yahoo.com.tw>
2023-09-13 15:59:57 +0800

    modify: product and productImage seeder

--- seeders/20230806113132-products-seed-file.js
+++ seeders/20230806113132-products-seed-file.js
@@ -1,25 +1,36 @@
 // PENDING: sku
 
 'use strict'
+const { faker } = require('@faker-js/faker')
 const SEED_PRODUCT = require('./product.json')
+const { Product, ProductImage } = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    SEED_PRODUCT.map(product => {
-      return queryInterface.bulkInsert('Products', [
-        {
-          name: product.name,
-          category_id: product.category_id,
-          cover: product.cover,
-          stock_quantity: product.stock_quantity,
-          price_regular: product.price_regular,
-          price_sale: product.price_sale,
-          description: product.description,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ])
-    })
+    for (const productData of SEED_PRODUCT) {
+      const product = await Product.create({
+        name: productData.name,
+        categoryId: productData.category_id,
+        cover: productData.cover,
+        stockQuantity: productData.stock_quantity,
+        priceRegular: productData.price_regular,
+        priceSale: productData.price_sale,
+        description: productData.description,
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+      const productId = product.id
+      const productImages = productData.images
+    
+      for (const image of productImages) {
+        await ProductImage.create({
+          productId: productId,
+          name: faker.word.adjective(),
+          imagePath: image.image_path,
+          is_display: image.is_display
+        })
+      }
+    }
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Products', {})
