1ae0c505602aecee2559d5daeeeb3a9e1a551de0
Yoruyeh <orchardsky31@gmail.com>
2025-02-06 14:13:26 +0800

    feat: add pagination info to get all products and searched products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,73 +1,82 @@
-const productService = require('../services/productService')
-const { CError } = require('../middleware/error-handler')
-const { isValidateId, isValidDate } = require('../helpers/validation-helper')
-const { getOneYearAgo, getToday } = require('../helpers/date-helper')
+const productService = require('../services/productService');
+const { CError } = require('../middleware/error-handler');
+const { isValidateId, isValidDate } = require('../helpers/validation-helper');
+const { getOneYearAgo, getToday } = require('../helpers/date-helper');
 
 const productController = {
-
   getProducts: async (req, res, next) => {
     try {
-      let { category, page, sort, keyword } = req.query
+      console.log(req.query);
+      let { category, page, limit, sort, keyword } = req.query;
       // page should be at least 1
-      page = page >= 1 ? page : 1
-      if (keyword && (typeof keyword !== "string" || keyword.length < 2)) {
-        throw new Error("Invalid search keyword")
+      page = page >= 1 ? page : 1;
+      if (keyword && (typeof keyword !== 'string' || keyword.length < 2)) {
+        throw new Error('Invalid search keyword');
       }
 
-      const { status, message, productCount, products } = await productService.getProducts(category, page, sort, keyword)
-      res.json({ status, message, productCount, products })
+      const { status, message, products, pagination } =
+        await productService.getProducts(category, page, limit, sort, keyword);
+      res.json({ status, message, products, pagination });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   getProduct: async (req, res, next) => {
     try {
-      const { id } = req.params
-      if (!id || !isValidateId(id)) throw new CError('invalid product id', 400)
+      const { id } = req.params;
+      if (!id || !isValidateId(id)) throw new CError('invalid product id', 400);
 
-      const { status, message, product } = await productService.getProduct(id)
-      res.json({ status, message, product })
+      const { status, message, product } = await productService.getProduct(id);
+      res.json({ status, message, product });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   searchProducts: async (req, res, next) => {
     try {
-      const { keyword } = req.query
-      if (!keyword || typeof keyword !== "string" || keyword.length < 2) {
-        throw new Error("Invalid search keyword")
+      let { page, keyword, limit } = req.query;
+      page = page >= 1 ? page : 1;
+      if (!keyword || typeof keyword !== 'string' || keyword.length < 2) {
+        throw new Error('Invalid search keyword');
       }
 
-      const { status, message, products } = await productService.searchProducts(keyword)
-      res.json({ status, message, products })
+      const { status, message, products, pagination } =
+        await productService.searchProducts(keyword, page, limit);
+      res.json({ status, message, products, pagination });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   getPopularProducts: async (req, res, next) => {
     try {
-      let { top, startDate, endDate, sort } = req.query
-      top = top >= 3 ? parseInt(top, 10) : 5
+      let { top, startDate, endDate, sort } = req.query;
+      top = top >= 3 ? parseInt(top, 10) : 5;
 
       // set default date
       if (!isValidDate(startDate)) {
-        startDate = getOneYearAgo()
+        startDate = getOneYearAgo();
       }
       if (!isValidDate(endDate)) {
-        endDate = getToday()
+        endDate = getToday();
       }
-      if (sort !== 'salesAmount' && sort !== 'salesQuantity' && sort !== '' && sort !== undefined) {
-        throw new CError('sort invalid', 400)
+      if (
+        sort !== 'salesAmount' &&
+        sort !== 'salesQuantity' &&
+        sort !== '' &&
+        sort !== undefined
+      ) {
+        throw new CError('sort invalid', 400);
       }
 
-      const { status, message, products } = await productService.getPopularProducts(top, startDate, endDate, sort)
-      res.json({ status, message, products })
+      const { status, message, products } =
+        await productService.getPopularProducts(top, startDate, endDate, sort);
+      res.json({ status, message, products });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-}
-module.exports = productController
+  },
+};
+module.exports = productController;
--- services/productService.js
+++ services/productService.js
@@ -1,26 +1,29 @@
-const sequelize = require('sequelize')
-const { Category, Product, ProductImage } = require('../models')
-const { Op } = require('sequelize')
+const sequelize = require('sequelize');
+const { Category, Product, ProductImage } = require('../models');
+const { Op } = require('sequelize');
 
 const productService = {
-  getProducts: async (category, page, sort, keyword) => {
+  getProducts: async (category, page, limit, sort, keyword) => {
     // define display products per page
-    const perPage = 9
+    const perPage = Number(limit) || 12;
     // define sorting options
     const sortOptions = {
       price_desc: ['price_regular', 'DESC'],
       price_asc: ['price_regular', 'ASC'],
-      date_desc: ['createdAt', 'DESC'],
-      date_asc: ['createdAt', 'ASC']
-    }
+      date_desc: ['created_at', 'DESC'],
+      date_asc: ['created_at', 'ASC'],
+    };
     // get product count
-    const productCount = await productService.getProductCount(category, keyword)
+    const productCount = await productService.getProductCount(
+      category,
+      keyword
+    );
 
     const queryOptions = {
       where: {},
       order: [],
       limit: perPage,
-      offset: (page - 1) * perPage,
+      offset: (Number(page) - 1) * perPage,
       attributes: [
         'id',
         'name',
@@ -33,55 +36,56 @@ const productService = {
       ],
       include: {
         model: Category,
-        attributes: ['name']
+        attributes: ['id', 'name'],
       },
       raw: true,
-      nest: true
-    }
+      nest: true,
+    };
     if (category) {
-      queryOptions.where.category_id = category
+      queryOptions.where.category_id = category;
     }
     if (sort && sortOptions[sort]) {
-      queryOptions.order.push(sortOptions[sort])
+      queryOptions.order.push(sortOptions[sort]);
     }
     if (keyword) {
       queryOptions.where[Op.or] = [
         {
           name: {
-            [Op.like]: `%${keyword}%`
-          }
-        }
-      ]
+            [Op.like]: `%${keyword}%`,
+          },
+        },
+      ];
     }
 
-    const products = await Product.findAll(queryOptions)
-    if (products.length) {
-      return {
-        status: 'success',
-        message: 'products retrieved succeed',
-        productCount,
-        products
-      }
-    } else {
-      return {
-        status: 'success',
-        message: 'no products found'
-      }
-    }
+    const products = await Product.findAll(queryOptions);
+    const paginationInfo = {
+      productCount,
+      currentPage: Number(page),
+      limit: perPage,
+      totalPage: Math.ceil(productCount / perPage),
+    };
+
+    return {
+      status: 'success',
+      message: products.length
+        ? 'products retrieved succeed'
+        : 'no products found',
+      products: products.length ? products : [],
+      pagination: paginationInfo,
+    };
   },
 
   getProductCount: async (categoryId, keyword) => {
-    const whereOptions = categoryId ? { category_id: categoryId } : {}
+    const whereOptions = categoryId ? { category_id: categoryId } : {};
     if (keyword) {
       whereOptions.name = {
-        [Op.like]: `%${keyword}%`
-      }
+        [Op.like]: `%${keyword}%`,
+      };
     }
+    let totalCount;
+    totalCount = await Product.count({ where: whereOptions });
 
-    let totalCount
-    totalCount = await Product.count({ where: whereOptions })
-
-    return totalCount
+    return totalCount;
   },
 
   getProduct: async (id) => {
@@ -93,129 +97,137 @@ const productService = {
         'price_regular',
         'price_sale',
         'description',
-        'stock_quantity'
+        'stock_quantity',
       ],
       include: {
         model: ProductImage,
         attributes: ['name', 'image_path'],
         where: {
-          is_display: 1
+          is_display: 1,
         },
-        required: false
+        required: false,
       },
-    })
+    });
     if (product !== null) {
       return {
         status: 'success',
         message: 'product retrieved succeed',
-        product
-      }
+        product,
+      };
     } else {
       return {
         status: 'success',
-        message: 'no product found'
-      }
+        message: 'no product found',
+      };
     }
   },
 
-  searchProducts: async (keyword) => {
+  searchProducts: async (keyword, page, limit) => {
+    const perPage = Number(limit) || 12;
+
     const products = await Product.findAll({
       where: {
         [Op.or]: [
           {
             name: {
-              [Op.like]: `%${keyword}%`
-            }
+              [Op.like]: `%${keyword}%`,
+            },
           },
           {
             description: {
-              [Op.like]: `%${keyword}%`
-            }
-          }
-        ]
+              [Op.like]: `%${keyword}%`,
+            },
+          },
+        ],
       },
+      limit: perPage,
+      offset: (Number(page) - 1) * perPage,
       attributes: ['id', 'category_id', 'name', 'cover'],
       include: {
         model: Category,
-        attributes: ['name']
-      }
-    })
+        attributes: ['id', 'name'],
+      },
+    });
 
-    if (products.length > 0) {
-      return {
-        status: 'success',
-        message: 'products retrieved succeed',
-        products
-      }
-    } else {
-      return {
-        status: 'success',
-        message: 'no products found'
-      }
-    }
+    // get product count
+    const productCount = await productService.getProductCount('', keyword);
+
+    const paginationInfo = {
+      productCount,
+      currentPage: Number(page),
+      limit: perPage,
+      totalPage: Math.ceil(productCount / perPage),
+    };
+
+    return {
+      status: 'success',
+      message: products.length
+        ? 'products retrieved succeed'
+        : 'no products found',
+      products: products.length ? products : [],
+      pagination: paginationInfo,
+    };
   },
 
   getPopularProducts: async (top, startDate, endDate, sort) => {
     let queryOptions = {
       limit: top,
-      attributes: [
-        'id',
-        'category_id',
-        'name',
-        'cover',
-        'price_regular',
-      ],
+      attributes: ['id', 'category_id', 'name', 'cover', 'price_regular'],
       include: {
         model: Category,
-        attributes: ['name']
+        attributes: ['name'],
       },
       raw: true,
-      nest: true
-    }
+      nest: true,
+    };
 
-    if (startDate !== undefined && endDate !== undefined) { 
-      queryOptions.where = { 
-        created_at: { 
-          [Op.between]: [startDate, endDate] 
-        }
-      }
+    if (startDate !== undefined && endDate !== undefined) {
+      queryOptions.where = {
+        created_at: {
+          [Op.between]: [startDate, endDate],
+        },
+      };
     }
 
     if (sort === 'salesAmount') {
       queryOptions.attributes.push([
         sequelize.cast(
-          sequelize.literal('(SELECT SUM(quantity * price_each) FROM `OrderItems` WHERE `OrderItems`.`product_id` = `Product`.`id`)'),
+          sequelize.literal(
+            '(SELECT SUM(quantity * price_each) FROM `OrderItems` WHERE `OrderItems`.`product_id` = `Product`.`id`)'
+          ),
           'DECIMAL(10, 2)'
         ),
-        'salesAmount'
-      ])
-      queryOptions.order = [[sequelize.literal('salesAmount'), 'DESC']]
+        'salesAmount',
+      ]);
+      queryOptions.order = [[sequelize.literal('salesAmount'), 'DESC']];
     } else {
       queryOptions.attributes.push([
         sequelize.cast(
-          sequelize.literal('(SELECT SUM(quantity) FROM `OrderItems` WHERE `OrderItems`.`product_id` = `Product`.`id`)'),
+          sequelize.literal(
+            '(SELECT SUM(quantity) FROM `OrderItems` WHERE `OrderItems`.`product_id` = `Product`.`id`)'
+          ),
           'SIGNED'
         ),
-        'salesCount'
-      ])
-      queryOptions.order = [[sequelize.literal('salesCount'), 'DESC']]
+        'salesCount',
+      ]);
+      queryOptions.order = [[sequelize.literal('salesCount'), 'DESC']];
     }
 
-    const products = await Product.findAll(queryOptions)
+    const products = await Product.findAll(queryOptions);
 
     if (products.length) {
       return {
         status: 'success',
         message: 'top products retrieved successfully',
-        products
-      }
+        products,
+      };
     } else {
       return {
         status: 'success',
-        message: 'no products found'
-      }
+        message: 'no products found',
+      };
     }
-  }
-}
+  },
+};
 
-module.exports = productService
+module.exports = productService;
